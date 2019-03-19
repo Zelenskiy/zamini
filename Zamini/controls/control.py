cb6f4e1737784d83d3b3c1171a8841ac4924d578
alexandr <Zelensk1973@gmail.com>
2019-03-19 21:40:56 +0200

    Повернув назад

--- Zamini/controls/control.py
+++ Zamini/controls/control.py
@@ -9,7 +9,7 @@ from PyQt5.QtGui import QPixmap
 
 from controls.funcRozklad import rowCol_to_dayPeriodTeacher
 
-from Zamini.controls.funcRozklad import cardId_to_groupId, equGrups
+from controls.funcRozklad import cardId_to_groupId, equGrups
 
 
 def testFunc():
@@ -140,34 +140,56 @@ def cell_clicked(self, roz, row, column):
                 c.lesson.teacherInThisLesson.append(teacher)
 
         self.ui.tableWidget2.setItem(row, column, kli[1])
-
-        #           проходимо по стовпчику і вилучаємо картки з даною групою/класом до списку
-        if self.ui.tableWidget2.item(row, column) != None:
-            s1 = self.ui.tableWidget2.item(row, column).text().strip()
-        else:
-            sl = ""
-        gr0 = cardId_to_groupId(roz, sl)
+        #           проходимо по стовпчику і вилучаємо картки з даним класом до списку
         for r in range(0, len(roz.teachers) - 1):
             if r == row:
                 continue
-            if self.ui.tableWidget2.item(r, column) != None:
-                s1 = self.ui.tableWidget2.item(r, column).text()
-                # Шукаємо картку
-                gr = cardId_to_groupId(roz, sl)
-                if equGrups(gr0, gr):
+            if kl[0] != "":
+                if self.ui.tableWidget.item(r, column) != None:
+                    s1 = self.ui.tableWidget.item(r, column).text()
+                    if s1.strip() == kl[0].strip():
+                        k_l = self.ui.tableWidget2.item(r, column)
+                        if k_l == None:
+                            k_l = ""
+                        else:
+                            k_l = k_l.text()
+                        roz.model.appendRow(QStandardItem(kl[0] + "                        &" + k_l))
+                        k_l = QtWidgets.QTableWidgetItem("")
+                        self.ui.tableWidget.setItem(r, column, k_l)
 
-
-
-
-
-                roz.model.appendRow(QStandardItem(kl[0] + "                        &" + k_l))
-                k_l = QtWidgets.QTableWidgetItem("")
-                self.ui.tableWidget.setItem(r, column, k_l)
-
-                self.ui.tableWidget2.setItem(r, column, k_l)
+                        self.ui.tableWidget2.setItem(r, column, k_l)
 
         # Вилучаємо зі списку зчитане значення
         roz.model.removeRow(self.roz.lv_index)
+
+        # #           проходимо по стовпчику і вилучаємо картки з даною групою/класом до списку
+        # if self.ui.tableWidget2.item(row, column) != None:
+        #     s1 = self.ui.tableWidget2.item(row, column).text().strip()
+        # else:
+        #     s1 = ""
+        # gr0 = cardId_to_groupId(roz, s1)
+        # for r in range(0, len(roz.teachers) - 1):
+        #     if r == row:
+        #         continue
+        #     if self.ui.tableWidget2.item(r, column) != None:
+        #         s1 = self.ui.tableWidget2.item(r, column).text()
+        #         # Шукаємо картку
+        #         gr = cardId_to_groupId(roz, sl)
+        #         if equGrups(gr0, gr):
+        #             pass
+        #
+        #
+        #
+        #
+        #
+        #         # roz.model.appendRow(QStandardItem(kl[0] + "                        &" + k_l))
+        #         # k_l = QtWidgets.QTableWidgetItem("")
+        #         # self.ui.tableWidget.setItem(r, column, k_l)
+        #         #
+        #         # self.ui.tableWidget2.setItem(r, column, k_l)
+        #
+        # # Вилучаємо зі списку зчитане значення
+        # roz.model.removeRow(self.roz.lv_index)
 
         if self.roz.lv_index > roz.model.rowCount() - 1:
             self.roz.lv_index = roz.model.rowCount() - 1
